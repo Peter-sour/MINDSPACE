d9b8423f81b5040d2ae25bbe606f938d94aec225
nadhiada <nadhiadewiartifasariabcd@gmail.com>
2025-05-14 15:05:38 +0700

    nambahin sm edit class

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -5,12 +5,12 @@ import '../styles/mood.css';
 function About() {
   return(
      <section id="about" class="section content user">
-        <div class="container">
-            <h2 class="section-title">About MindSpace</h2>
-            <p class="section-subtitle">A safe space dedicated to teen mental health awareness, education, and support</p>
+        <div className="container">
+            <h2 className="section-title">About MindSpace</h2>
+            <p className="section-subtitle">A safe space dedicated to teen mental health awareness, education, and support</p>
 
-            <div class="mood-tracker reveal-from-top">
-                <h3 class="mood-title">How are you feeling today?</h3>
+            <div className="mood-tracker reveal-from-top">
+                <h3 className="mood-title">How are you feeling today?</h3>
                 <MoodTracker />
             </div>
         </div>
